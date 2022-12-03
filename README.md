acfcd84e6af592b87e4e720e858b262c1c5bb328
bzhn <bzhn@duck.com>
2022-12-04 01:18:59 +0200

    Now it's working. Bot sends messages about connections to the channel

---
+++ README.md
@@ -0,0 +1,44 @@
+Telegram Bot which reads configs from ufw and sends notifications there are blocked/audited/allowed connections. Use with sudo.
+
+Bot reads config file, parses path to file to follow, telegram bot API token, channel to send updates to and something else. After that bot uses this information to wait for new changes in the log file and sends messages to the channel in pretty format.
+
+# Usage
+
+## Setup UFW firewall
+
+### Add rules to UFW
+
+### Enable UFW
+
+### Restart server (optional)
+
+## Setup Telegram Bot
+
+### Create Telegram Bot
+
+### Copy Bot API Token
+
+### Create channel with logs
+
+### Check channel's ID
+
+Send a message to the new channel and copy link of the message. You'll find a channel ID in the link (between two slashes). Add `-100` before the ID and note it for the future.
+
+## Build project
+
+Just clone this project to your server and run `go build`. Otherwise you can use prebuilt binary from releases (if it persists).
+
+## Setup config
+
+Add the Bot API token and channel's ID to the config. Use `config-example.yml` template from the root of the repository.
+
+Don't forget to rename a config file.
+
+Add vital ports, which are important ports you want to distinguish in the channel with updates.
+
+## Run the program
+
+1. Run the executable with sudo
+2. Use `nohup` or something like that to keep program running even if you're exiting from the server.
+3. Check if everything is working.
+
---
+++ main.go
@@ -0,0 +1,265 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+
+	"github.com/fsnotify/fsnotify"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/spf13/viper"
+	"go.xela.tech/abuseipdb"
+)
+
+// type UFWAction int
+
+// const (
+// 	ALLOW UFWAction = 1 << iota
+// 	AUDIT
+// 	BLOCK
+// 	UNKNOWN
+// )
+
+// One line of a message from ufw firewall
+type UfwLogMsg struct {
+	Action     string
+	IsInput    bool
+	Proto      string
+	SourceIP   string
+	SourcePort int
+	DestIP     string
+	DestPort   int
+	DateTime   string
+}
+
+func (m UfwLogMsg) String() string {
+	importanceLine := func(b bool) string {
+		if b {
+			return fmt.Sprintf("‼ <code>%d</code> was pinged by <code>%s</code>\n", m.DestPort, m.SourceIP)
+		}
+		return ""
+	}(isVitalPort(m.DestPort))
+	return fmt.Sprintf("%s<code>%s</code> → <code>%d</code> [%s] \n\nAction:\t<code>%s</code>\nSource:\t<code>%s:%d</code>\nDest:\t<code>%s:%d</code>", importanceLine, m.SourceIP, m.DestPort, m.Action, m.Action, m.SourceIP, m.SourcePort, m.DestIP, m.DestPort)
+}
+
+func isVitalPort(port int) bool {
+	for _, p := range VitalPorts {
+		if p == port {
+			return true
+		}
+	}
+	return false
+}
+
+var MyVPSIP string          // IP of my VPS. Used when checking wether connection is incoming or outcoming
+var abRep *abuseipdb.Client // Client for AbuseIPDB.com for reporting malicious IP's
+var FileToFollow string     // Paths to file which will be monitored
+var VitalPorts []int        // Ports which will be marked as important when somebody will try to connect through it. Used in UfwLogMsg.String()
+var TgBotAPIToken string    // Telegram Bot API token
+var ChannelToPub int64      // Channel where new messages will be sent
+var BotClient *tgbotapi.BotAPI
+
+// errors
+var (
+	ErrNoMatches = errors.New("Providen string doesn't have any matches with regex")
+)
+
+var re *regexp.Regexp
+
+func init() {
+	var err error
+	re, err = regexp.Compile(`(?P<datetime>[a-zA-Z]{2,4}[\s]{1,4}[\d]{1,2}[\s][\d]{2}:[\d]{2}:[\d]{2})[\s](vm[\d]{2,10}) [\w]{1,14}:[\s]\[ {0,10}[\d.]{1,50} {0,10}] \[UFW (?P<ufwaction>[A-Z]{1,14})] IN=(?:ens3)? OUT=(?:ens3)? MAC=(?:(?:[a-f0-9]{2}:){13}[a-f0-9]{2}) SRC=(?P<sip>(:?(:?25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(:?25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)) DST=(?P<dip>(:?(:?25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(:?25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)) LEN=[\d]{1,10} TOS=0x[\d]+ PREC=0x[\d]{1,10} TTL=[\d]+ ID=[\d]+ (:?DF )?PROTO=(?P<proto>TCP|UDP) SPT=(?P<sport>[\d]{1,5}) DPT=(?P<dport>[\d]{1,5}).*`)
+	if err != nil {
+		log.Panic("Can't compile regex", err)
+	}
+
+	viper.SetConfigFile("config.yml")
+	err = viper.ReadInConfig()
+	if err != nil {
+		log.Println(err)
+	}
+
+	// parse yaml config file
+	abRepApi := viper.GetString("abuseipapikey")
+	MyVPSIP = viper.GetString("vpsip")
+	FileToFollow = viper.GetString("file")
+	VitalPorts = viper.GetIntSlice("vitalports")
+	TgBotAPIToken = viper.GetString("TgBotAPI")
+	ChannelToPub = viper.GetInt64("ChannelToPub")
+
+	log.Printf("\n%s\n%s\n%s\n%v\n%d\n", abRepApi, MyVPSIP, FileToFollow, VitalPorts, ChannelToPub)
+
+	abRep = abuseipdb.NewClient(abRepApi)
+	log.Printf("Connected to AbuseIPDB")
+
+	// Connect to Telegram Bot API
+	BotClient, err = tgbotapi.NewBotAPI(TgBotAPIToken)
+	if err != nil {
+		log.Panic(err)
+	}
+	BotClient.Debug = true
+	log.Println("Authorised bot with username", BotClient.Self.UserName)
+}
+
+func ParseMsg(msg string) (UfwLogMsg, error) {
+	matches := re.FindStringSubmatch(msg)
+	// log.Println("Got string", msg)
+	if len(matches) == 0 {
+		log.Println("No regex matches in string. Continue seeking")
+		return UfwLogMsg{}, ErrNoMatches
+	}
+	log.Print("\t\t\tParsed regex with ", len(matches), " elements")
+	mm, err := matchesToMap(re, matches)
+	// checkError(err)
+	// fmt.Printf("%v\n", mm)
+	ulm := UfwLogMsg{
+		Action: mm["ufwaction"],
+		IsInput: func() bool {
+			if mm["sport"] == MyVPSIP {
+				return false
+			}
+			return true
+		}(),
+
+		Proto:      mm["proto"],
+		SourceIP:   mm["sip"],
+		SourcePort: tryAtoi(mm["sport"]),
+		DestIP:     mm["dip"],
+		DestPort:   tryAtoi(mm["dport"]),
+		DateTime:   mm["datetime"],
+	}
+
+	return ulm, err
+}
+
+func matchesToMap(reg *regexp.Regexp, matches []string) (map[string]string, error) {
+	regexNnames := reg.SubexpNames()
+	if len(regexNnames) != len(matches) {
+		return map[string]string{}, errors.New("Len of matches and regexp submatches doesn't match.")
+	}
+	res := make(map[string]string)
+	for i, v := range matches {
+		if len(regexNnames[i]) == 0 {
+			res[fmt.Sprintf("%d", i)] = v
+			continue
+		}
+
+		res[regexNnames[i]] = v
+	}
+
+	return res, nil
+}
+
+func tryAtoi(s string) (n int) {
+	n, _ = strconv.Atoi(s)
+	return
+}
+
+func SendIfOk(ufw UfwLogMsg) {
+	log.Println("Checking if the message can be sent... Connection is", fmt.Sprintf("%s -> %d", ufw.SourceIP, ufw.DestPort))
+	if ufw.IsInput == false {
+		log.Println("The connection is outcoming. Return")
+		return
+	}
+
+	msg := tgbotapi.NewMessage(ChannelToPub, ufw.String())
+	msg.ParseMode = tgbotapi.ModeHTML
+	_, err := BotClient.Send(msg)
+	if err != nil {
+		log.Print(err)
+	}
+	log.Printf("Sent message to channel")
+}
+
+func main() {
+
+	// Create new watcher.
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	// Start listening for events.
+	go func() {
+		for {
+			// log.Println("New loop")
+			select {
+			case event, ok := <-watcher.Events:
+				if err != nil {
+					log.Fatal(err)
+				}
+				// log.Println("Event case")
+				if !ok {
+					log.Println("Not ok")
+					return
+				}
+				// Got a new message
+				if event.Has(fsnotify.Write) {
+					msg := ReadLastLine(FileToFollow)
+					parsed, err := ParseMsg(msg)
+					if err != nil {
+						// log.Println(err)
+						continue
+					}
+					// log.Println(parsed)
+					SendIfOk(parsed)
+				}
+			case err, ok := <-watcher.Errors:
+
+				// log.Println("Got an error", err)
+				if !ok {
+					log.Println("Not ok")
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	// Add a path.
+	// err = watcher.Add("/home/dev/Downloads/changeme.txt")
+	err = watcher.Add(FileToFollow)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Added new file to watch")
+
+	// Block main goroutine forever.
+	<-make(chan struct{})
+}
+
+func ReadLastLine(filepath string) string {
+	f, err := os.Open(filepath)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return ""
+	}
+	defer f.Close()
+	buf := make([]byte, 500)
+	stats, err := os.Stat(filepath)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return ""
+	}
+	start := stats.Size() - 500
+	_, err = f.ReadAt(buf, start)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return ""
+	}
+
+	rows := bytes.Split(buf, []byte("\n"))
+
+	return string(rows[len(rows)-2:][0])
+}
+
+func checkError(e error) {
+	if e != nil {
+		log.Println(e)
+	}
+}
